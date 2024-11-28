76576d034413cdbbecb62ff156b97318197b9f40
Christophe Thibaut <cthibauttof@gmail.com>
2024-11-28 15:49:14 +0100

    draw a 4x4 pixel block

--- src/pentomino.s
+++ src/pentomino.s
@@ -1,23 +1,33 @@
 ; test values
     lda #$05
     sta $0F
-    lda #$04
+    lda #$00
     sta $10
     lda #$00
     sta $11
 
 ; draw a 4x4 pixel block on top left corner
 ; color in $OF; row in $10 col in $11
-    lda #$00
-    sta $13
-    lda $10
+; block location in $12-13
+
+    lda #$00 ; base address = $200
     sta $12
-    ldy #$07 ; multiply row by 64
-mult_64:
-    asl $12
-    rol $13
+    lda #$02
+    sta $13
+    lda $10 ; row > 0 ? add row offset
+    beq add_col
+    tay
+add_row:
+    clc
+    lda $12
+    adc #$80
+    sta $12
+    lda $13
+    adc #$00
+    sta $13
     dey
-    bne mult_64
+    bne add_row
+add_col:
     asl $11 ; multiply col by 4
     asl $11
     clc
@@ -25,31 +35,22 @@ mult_64:
     adc $11
     sta $12
     lda $13
-    lda #$00
+    adc #$00
     sta $13
-    clc
-    lda $13
-    adc #$02 ; add screen base address
-    sta $13
-    ldy #$00
+lines:
     jsr line
-    ldy #$00
     jsr line
-    ldy #$00
     jsr line
-    ldy #$00
     jsr line
     rts
 
 line:
-    lda $0F
+
+    ldy #$03
+pixels:
     jsr pixel
-    iny
-    jsr pixel
-    iny
-    jsr pixel
-    iny
-    jsr pixel
+    dey
+    bpl pixels
     clc
     lda $12
     adc #$20
@@ -59,7 +60,7 @@ line:
     sta $13
     rts
 pixel:
+    lda $0F
     sta ($12),y
     rts
-
 
