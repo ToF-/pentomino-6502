ce30af659153e258de1f6ab22637c80c0d676ad7
Christophe Thibaut <cthibauttof@gmail.com>
2024-11-28 13:56:29 +0100

    draw a block

--- src/pentomino.s
+++ src/pentomino.s
@@ -1,8 +1,65 @@
-; draw a 4 pixel block on top left corner
-
+; test values
     lda #$05
-    sta $0200
-    sta $0201
-    sta $0220
-    sta $0221
+    sta $0F
+    lda #$04
+    sta $10
+    lda #$00
+    sta $11
+
+; draw a 4x4 pixel block on top left corner
+; color in $OF; row in $10 col in $11
+    lda #$00
+    sta $13
+    lda $10
+    sta $12
+    ldy #$07 ; multiply row by 64
+mult_64:
+    asl $12
+    rol $13
+    dey
+    bne mult_64
+    asl $11 ; multiply col by 4
+    asl $11
+    clc
+    lda $12 ; add to destination
+    adc $11
+    sta $12
+    lda $13
+    lda #$00
+    sta $13
+    clc
+    lda $13
+    adc #$02 ; add screen base address
+    sta $13
+    ldy #$00
+    jsr line
+    ldy #$00
+    jsr line
+    ldy #$00
+    jsr line
+    ldy #$00
+    jsr line
     rts
+
+line:
+    lda $0F
+    jsr pixel
+    iny
+    jsr pixel
+    iny
+    jsr pixel
+    iny
+    jsr pixel
+    clc
+    lda $12
+    adc #$20
+    sta $12
+    lda $13
+    adc #$00
+    sta $13
+    rts
+pixel:
+    sta ($12),y
+    rts
+
+
